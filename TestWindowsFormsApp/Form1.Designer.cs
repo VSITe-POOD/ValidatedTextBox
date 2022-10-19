af2162773cb033092eedd8f1462c937811e2f8ba
Julijan Sribar <julijan.sribar@gmail.com>
2022-10-19 20:25:02 +0200

    Implement LettersOnlyTextBox

--- TestWindowsFormsApp/Form1.Designer.cs
+++ TestWindowsFormsApp/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.numericTextBox1 = new MyEditControls.NumericTextBox();
+            this.lettersOnlyTextBox1 = new MyEditControls.LettersOnlyTextBox();
             this.SuspendLayout();
             // 
             // textBox1
@@ -55,11 +56,19 @@
             this.numericTextBox1.Size = new System.Drawing.Size(152, 20);
             this.numericTextBox1.TabIndex = 2;
             // 
+            // lettersOnlyTextBox1
+            // 
+            this.lettersOnlyTextBox1.Location = new System.Drawing.Point(12, 126);
+            this.lettersOnlyTextBox1.Name = "lettersOnlyTextBox1";
+            this.lettersOnlyTextBox1.Size = new System.Drawing.Size(152, 20);
+            this.lettersOnlyTextBox1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lettersOnlyTextBox1);
             this.Controls.Add(this.numericTextBox1);
             this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.textBox1);
@@ -75,6 +84,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private MyEditControls.NumericTextBox numericTextBox1;
+        private MyEditControls.LettersOnlyTextBox lettersOnlyTextBox1;
     }
 }
 
